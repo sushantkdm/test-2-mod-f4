e9a039bb3326d3d525804dfb9ee9755252234b83
sushantkdm <sushantkadam936@gmail.com>
2023-05-20 18:07:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component from './Component';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Table from './Table';
+
+const Component = () => {
+  const [data, sethata] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false', { crossDomain: 'true' });
+        
+        const jsonData = await response.json();
+        console.log(jsonData)
+        sethata(jsonData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+   <table>
+   <thead>
+   <tr>
+      <th>Image</th>
+      <th>Name</th>
+      <th>Symbol</th>
+      <th>Current Price</th>
+      <th>Total Volume</th>
+      <th>Price Change</th>
+      <th>Market Cap</th>
+      </tr>
+  </thead>
+  <tbody>
+    {data.map(item => (
+      <Table key={item.id} data={item} />
+    ))}
+    </tbody>
+  </table>
+  );
+};
+
+export default Component;
